15355eb73719af1c8220f55e1eaef313c7e23c11
HakanOzsoy <75271013+HakanOzsoy@users.noreply.github.com>
2022-09-13 20:27:54 +0300

    Add files via upload

---
+++ WebApplication2/Security/MyRoles.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebApplication2.Models;
+
+namespace WebApplication2.Security
+{
+    public class MyRoles : RoleProvider
+    {
+        public override string ApplicationName {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            string[] roller = null;
+            YemekSiparisEntities n = new YemekSiparisEntities();
+            Kullanici user = n.Kullanici.FirstOrDefault(x => x.kullaniciAdi == username);
+            string rol = user.rol;
+
+            char[] rolChar = rol.ToCharArray();
+            roller = new string[rolChar.Length];
+            for(int i = 0; i < rol.Length; i++)
+            {
+                roller[i] = rolChar[i].ToString();
+            }
+
+            return roller;
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication2/Security/UserID.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Security
+{
+    public static class UserID
+    {
+        static YemekSiparisEntities n = new YemekSiparisEntities();
+
+        public static bool kullaniciID(string name, int resID)
+        {
+            Kullanici k = n.Kullanici.FirstOrDefault(x => x.kullaniciAdi == name);
+            Restoran r = n.Restoran.FirstOrDefault(x => x.RestoranKodu == resID);
+            if(k == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (r.YetkiliID == k.Id)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }                   
+        }
+
+        public static int idBul(string name)
+        {
+            Kullanici k = n.Kullanici.FirstOrDefault(x => x.kullaniciAdi == name);
+            return k.Id;
+        }
+
+        public static string adresBul (int id)
+        {
+            Kullanici k = n.Kullanici.FirstOrDefault(x => x.Id == id);
+            Adres a = n.Adres.FirstOrDefault(x => x.AdresKodu == k.AdresKodu);
+
+            return a.DetayliAdres;
+        }
+    }
+}
